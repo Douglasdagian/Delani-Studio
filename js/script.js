135c4d3b2d61f3dbd9930c85597309fa31564c10
Douglas Evole <douglaseamutete@gmail.com>
2020-10-12 00:09:39 +0300

    add alert on js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ $(".image").click(function () {
   $(this).children(".show").slideToggle();
   $(this).children("#show").toggle();
 });
+
+
+$(document).ready(function(){
+  $("form").submit(function(){
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var text = $("textarea#hen").val();
+    if ($("input#name").val() && $("input#email").val()){
+      alert(name + "thank you for your feedback");
+    }
+  });
+});
