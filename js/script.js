80eeba0f6801be33133379fec051ca23b725a40b
Douglas Evole <douglaseamutete@gmail.com>
2020-10-14 21:38:27 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-$(".image").click(function () {
-  $(this).children(".show").slideToggle();
-  $(this).children("#show").toggle();
-});
+// $(".image").click(function () {
+//   $(this).children(".show").slideToggle();
+//   $(this).children("#show").toggle();
+// });
 
 
-$(document).ready(function(){
-  $("form").submit(function(){
-    var name = $("input#name").val();
-    var email = $("input#email").val();
-    var text = $("textarea#hen").val();
-    if ($("input#name").val() && $("input#email").val()){
-      alert(name + "thank you for your feedback");
-    }
-  });
-});
+// $(document).ready(function(){
+//   $("form").submit(function(){
+//     var name = $("input#name").val();
+//     var email = $("input#email").val();
+//     var text = $("textarea#hen").val();
+//     if ($("input#name").val() && $("input#email").val()){
+//       alert(name + "thank you for your feedback");
+//     }
+//   });
+// });
 
-$(document).ready(function(){
-  $(".mask1").hover(function(){
-    $("#us1").toggle();
-  });
-  $(".img1").hover(function(){
-    $(".mg1").toggle();
-  });
-  $(".mask3").hover(function(){
-    $("#us3").toggle();
-  });
-  $(".mask4").hover(function(){
-    $("#us4").toggle();
-  });
-  $(".mask5").hover(function(){
-    $("#us5").toggle();
-  });
-  $(".mask6").hover(function(){
-    $("#us6").toggle();
-  });
-  $(".mask7").hover(function(){
-    $("#us7").toggle();
-  });
-  $("#img3").hover(function(){
-    $(".overlay").toggle();
-  });
-});
+// $(document).ready(function(){
+//   $(".mask1").hover(function(){
+//     $("#us1").toggle();
+//   });
+//   $(".img1").hover(function(){
+//     $(".mg1").toggle();
+//   });
+//   $(".mask3").hover(function(){
+//     $("#us3").toggle();
+//   });
+//   $(".mask4").hover(function(){
+//     $("#us4").toggle();
+//   });
+//   $(".mask5").hover(function(){
+//     $("#us5").toggle();
+//   });
+//   $(".mask6").hover(function(){
+//     $("#us6").toggle();
+//   });
+//   $(".mask7").hover(function(){
+//     $("#us7").toggle();
+//   });
+//   $("#img3").hover(function(){
+//     $(".overlay").toggle();
+//   });
+// });
